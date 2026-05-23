14fbbd4b7664820fea6b72096a0ea1a5a036f0f0
Пушменкова Анастасия Алексеевна <nastyapushmenkova@yandex.ru>
2026-05-23 10:59:43 +0300

    Задание 11.1: добавилазависимость test для написания тестов

--- dartpedia/wikipedia/lib/wikipedia.dart
+++ dartpedia/wikipedia/lib/wikipedia.dart
@@ -1,3 +1,7 @@
-int calculate() {
-  return 6 * 7;
-}
+/// Models for Wikipedia API responses.
+library;
+
+export 'src/model/article.dart';
+export 'src/model/search_results.dart';
+export 'src/model/summary.dart';
+export 'src/model/title_set.dart';
