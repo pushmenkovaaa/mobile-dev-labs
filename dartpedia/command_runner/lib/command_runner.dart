68ab2c692d4c0ef3af24d106010438fbfdfeaf05
Пушменкова Анастасия Алексеевна <nastyapushmenkova@yandex.ru>
2026-05-23 09:53:42 +0300

    Задание 6.3: добавила экспорт для arguments и help_command

--- dartpedia/command_runner/lib/command_runner.dart
+++ dartpedia/command_runner/lib/command_runner.dart
@@ -3,6 +3,6 @@
 /// More extensive documentation for this library goes here.
 library;
 
+export 'src/arguments.dart';
 export 'src/command_runner_base.dart';
-// TODO: Export any other libraries intended for clients of this package.
-
+export 'src/help_command.dart';
