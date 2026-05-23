c15bb225cd7c77f99bfb08bb7a5e0576e5d8428a
Пушменкова Анастасия Алексеевна <nastyapushmenkova@yandex.ru>
2026-05-23 10:04:07 +0300

    Задание 7.2: добавила onError обработчик в commandrunner

--- dartpedia/command_runner/lib/src/command_runner_base.dart
+++ dartpedia/command_runner/lib/src/command_runner_base.dart
@@ -1,9 +1,20 @@
+import 'dart:async'; 
 import 'dart:collection';
 import 'dart:io';
 import 'arguments.dart';
+import 'exceptions.dart'; 
 
 class CommandRunner {
+   
+    CommandRunner({this.onError});
+
   final Map<String, Command> _commands = <String, Command>{};
+
+  UnmodifiableSetView<Command> get commands =>
+      UnmodifiableSetView<Command>(<Command>{..._commands.values});
+
+  // ← Добавить свойство:
+  FutureOr<void> Function(Object)? onError;
 
   UnmodifiableSetView<Command> get commands =>
       UnmodifiableSetView<Command>(<Command>{..._commands.values});
