51b1a8328cf3614f47ceccff63fa9f811df1c6fa
Пушменкова Анастасия Алексеевна <nastyapushmenkova@yandex.ru>
2026-05-30 10:51:59 +0300

    Перенесли logger.dart на уровень выше, так как созала не там, добавила проверку на пустое значение, так как изначально не создавались логи при пустом запросе

--- dartpedia/cli/lib/src/commands/search.dart
+++ dartpedia/cli/lib/src/commands/search.dart
@@ -43,6 +43,11 @@ class SearchCommand extends Command {
     try {
       final SearchResults results = await search(args.commandArg!);
 
+      if (results.results.isEmpty) {
+        logger.info('No results found for: ${args.commandArg}');
+        return 'No articles found for "${args.commandArg}"';
+      }
+
       if (args.flag('im-feeling-lucky')) {
         final title = results.results.first.title;
         final Summary article = await getArticleSummaryByTitle(title);
